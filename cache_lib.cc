fa121211dc88af0e7fbbfcbe606064f6026d33e1
gopalanvinay <gopalanvinay@gmail.com>
2020-03-01 13:39:20 -0800

    Fixed class member bugs and formatting

--- cache_lib.cc
+++ cache_lib.cc
@@ -3,7 +3,11 @@
 #include <unordered_map>
 
 class Cache::Impl {
-public:
+ private:
+   // data structure
+   std::unordered_map<key_type, val_type> table;
+
+ public:
     // Add a <key, value> pair to the cache.
     // If key already exists, it will overwrite the old value.
     // Both the key and the value are to be deep-copied (not just pointer copied).
@@ -13,7 +17,7 @@ public:
     void set(key_type key, val_type val, size_type size) {
         if (space_used() + sizeof(val_type) < size)  {
             val_type to_copy = val;
-            table[key] = &to_copy; // check this behavior
+            table[key] = to_copy; // check this behavior
         }
         // don't add into hash table
     }
@@ -22,9 +26,10 @@ public:
     // or nullptr if not found.
     // Sets the actual size of the returned value (in bytes) in val_size.
     val_type get(key_type key, size_type& val_size) const {
+        // table.at() returns a reference to the value
         if (table.at(key) != nullptr) {
-            val_size = sizeof(&table[key]);
-            return &table[key];
+            val_size = sizeof(table.at(key));
+            return table.at(key); // check pointer v reference here
         } else {
             return nullptr;
         }
@@ -51,40 +56,53 @@ public:
             assert(del(it.first) ==  true);
         }
     }
-
-private:
-   // data structure
-   std::unordered_map<key_type, val_type*> table;
 };
 
 // cache impl stuff
 
 // constructor for the cache
 
-Cache::Cache() : pImpl_(new Cache::Impl) {}
+Cache::Cache(size_type maxmem,
+            float max_load_factor,
+            Evictor* evictor,
+            hash_func hasher)
+    : pImpl_(new Impl()) {}
 
-void Cache::set(key_type key, val_type val, size_type size) {
+void
+Cache::set(key_type key, val_type val, size_type size) {
     Cache::pImpl_->set(key, val, size);
 }
 
 // Retrieve a pointer to the value associated with key in the cache,
 // or nullptr if not found.
 // Sets the actual size of the returned value (in bytes) in val_size.
-Cache::val_type get(key_type key, Cache::size_type& val_size) const {
-    Cache::pImpl_->get(key, val_size);
+Cache::val_type
+Cache::get(key_type key, Cache::size_type& val_size) const {
+    return Cache::pImpl_->get(key, val_size);
 }
 
 // Delete an object from the cache, if it's still there
-bool Cache::del(key_type key){
-    Cache::pImpl_->del(key);
+bool
+Cache::del(key_type key){
+    return Cache::pImpl_->del(key);
 }
 
 // Compute the total amount of memory used up by all cache values (not keys)
-Cache::size_type space_used() const {
-    Cache::pImpl_->space_used();
+Cache::size_type
+Cache::space_used() const {
+    return Cache::pImpl_->space_used();
 }
 
 // Delete all data from the cache
-void Cache::reset(){
+void
+Cache::reset(){
     Cache::pImpl_->reset();
+}
+
+
+// Main
+int
+main()
+{
+    return 0;
 }
