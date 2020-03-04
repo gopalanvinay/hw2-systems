36e53b8fddbea3efd57faad03b2f2b8d4ddc594d
gopalanvinay <gopalanvinay@gmail.com>
2020-03-03 21:04:03 -0800

    Added more tests for Part 1

--- test_cache_lib.cc
+++ test_cache_lib.cc
@@ -2,7 +2,7 @@
 #include <assert.h>
 
 
-int test_set(Cache &obj, uint32_t &val_size)
+void test_set_and_get(Cache &obj, Cache::size_type& val_size)
 {
     obj.set("a","1",25);
     assert(obj.get("a", val_size) == "1");
@@ -13,16 +13,44 @@ int test_set(Cache &obj, uint32_t &val_size)
     obj.set("d","7",25);
     // maxmem exceeded
     assert(obj.get("d", val_size) == nullptr);
-    printf("All set tests passed!\n");
-    return 0;
+    printf("All set and get tests passed!\n");
 }
 
+void test_del(Cache &obj, Cache::size_type& val_size)
+{
+
+    obj.set("testDel1","1",10);
+    assert(obj.get("testDel1",val_size) == "1");
+    obj.del("testDel1");
+    assert(obj.get("testDel1",val_size) == nullptr);
+    obj.set("testDel2","4",40);
+    assert(obj.get("testDel2",val_size) == "4");
+    obj.del("testDel2");
+    assert(obj.get("testDel2",val_size) == nullptr);
+    printf("All del tests passed!\n");
+}
+
+void test_space_used(Cache &empty_obj)
+{
+    empty_obj.set("testSpace","3",8);
+    assert(empty_obj.space_used() == 8);
+    empty_obj.del("testSpace");
+    assert(empty_obj.space_used() == 0);
+    printf("All space use tests passed!\n");
+}
 
 // Main
 int main()
 {
-    uint32_t a;
-    Cache x(25);
-    test_set(x, a);
+    Cache::size_type a;
+    Cache obj_set(25);
+    test_set_and_get(obj_set, a);
+
+    Cache obj_del(10);
+    test_del(obj_del, a);
+
+    Cache empty_obj(10);
+    test_space_used(empty_obj);
+
     return 0;
 }
